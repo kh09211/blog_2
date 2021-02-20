05af0460beab5db5d8fcff1016694d7affd4a6b4
Kyle <kh09211@georgiasouthern.edu>
2021-02-20 16:12:29 -0500

    added login and register templates

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -6,9 +6,21 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class SecurityController extends AbstractController
 {
+
+    // private property used for encoding passwords
+    private $passwordEncoder;
+
+    // constructor function for injecting the password encoder interface
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder) {
+
+        $this->passwordEncoder = $passwordEncoder;
+    }
+    
+
     /**
      * @Route("/login", name="app_login")
      */
@@ -33,4 +45,37 @@ class SecurityController extends AbstractController
     {
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
     }
+
+    /**
+     * Render the register template
+     * 
+     * @Route("/register", methods={"get"}, name="app_register")
+     */
+    public function register(): Response
+    {
+        // if ($this->getUser()) {
+        //     return $this->redirectToRoute('target_path');
+        // }
+
+        return $this->render('security/register.html.twig', []);
+    }
+
+    /**
+     *  get method will store user data and encode a password using user entity
+     * 
+     * @Route("/register", methods={"post"}, name="app_register_store")
+     */
+    public function store(): Response
+    {
+        // create a new user instance then input the data then persist it to the database
+        // lastly redirect the user to their personal blog route showing the create a blog button
+
+
+        // if ($this->getUser()) {
+        //     return $this->redirectToRoute('target_path');
+        // }
+
+        return $this->render('security/register.html.twig', []);
+    }
+
 }
